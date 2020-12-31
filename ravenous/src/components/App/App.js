407111cf5016917edfa129fcc3e31469893116ca
hannah huang <hannahhuang000@outlook.com>
2020-12-31 11:58:40 -0500

    finish part 2

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -6,16 +6,42 @@ import Business from '../Business/Business';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+//paste Business object
+import { render } from 'react-dom';
+import '../Business/Business.css';
+
+ const business = {
+    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+ }
 
 
-function App() {
+const a = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]; 
+
+class App extends React.Component {
+  render(){
   return (
         <div className="App">
           <h1>ravenous</h1>
           <SearchBar />
-          <BusinessList />
+          <BusinessList a={a}/>
         </div>
-  )
+  );
+}
 }
 
 export default App;
